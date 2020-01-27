405754d9f4ecc3305aecddd4dfb799a3ff5c05e2
Nikitha Keerthana <58261586+nikki-design@users.noreply.github.com>
2020-01-27 15:47:10 +0530

    Create training8.c

---
+++ training8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int i=65;
+ switch(i)
+ {
+ case 65:
+ printf("Integer 65");
+ break;
+ case 'A':
+ printf("Char 65");
+ default:
+ printf("Bye");
+ }
+return 0;
+}
